c69c3d520b2e0c9d4e3abb40c96fc4c9786a1f32
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-07-08 18:29:20 +0100

    update: update the UserContent component

--- frontend/src/components/UserContent.tsx
+++ frontend/src/components/UserContent.tsx
@@ -1,7 +1,5 @@
-// components/UserContent.tsx
-import React from 'react';
-import { Form, Button, Card, Image, Row, Col, InputGroup } from 'react-bootstrap';
-import { FaSearch, FaEnvelope, FaUserPlus } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 import TopBar from './TopBar';
 import { AuthContextType } from './VisitorClient';
@@ -10,37 +8,36 @@ import About from './About';
 import Feed from './Feed';
 import OtherClients from './OtherClients';
 
-interface UserContextProps {
-    authUser: AuthContextType
+interface UserContentProps {
+    authUser: AuthContextType;
+    currentUser: AuthContextType;
+    storedUser: AuthContextType;
+    setCurrentUser: React.Dispatch<React.SetStateAction<AuthContextType>>
 }
 
-const FaSearchIcon = FaSearch as unknown as React.FC<React.SVGProps<SVGSVGElement>>;
-const FaEnvelopeIcon = FaEnvelope as unknown as React.FC<React.SVGProps<SVGSVGElement>>;
-const FaUserPlusIcon = FaUserPlus as unknown as React.FC<React.SVGProps<SVGSVGElement>>;
 
-const UserContent: React.FC<UserContextProps> = ({authUser}) => {
+const UserContent: React.FC<UserContentProps> = ({ storedUser, authUser, currentUser, setCurrentUser }) => {
+//   const [currentUser, setCurrentUser] = useState(authUser);
+
   return (
-    <div className="p-5 w-100" style={{margin: 'auto', backgroundColor: 'rgba(248,248,248, 0.2 )'}}>
+    <div className="p-5 w-100" style={{ margin: 'auto', backgroundColor: 'rgba(248,248,248, 0.2 )' }}>
       {/* Top Bar */}
-        <TopBar authUser={authUser} />
+      <TopBar currentUser={currentUser} setCurrentUser={setCurrentUser} authUser={authUser} />
 
       {/* Profile Header */}
-      
-        <ProfileHeader authUser={authUser}/>
+      <ProfileHeader storedUser={storedUser} authUser={authUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
 
-        <Row style={{color: ' #a303a0'}} className="gx-2">
-            <Col xs={12} md={4} lg={4}>
-                <About authUser={authUser} />
-            </Col>
-            <Col xs={12} md={6} lg={4}>
-                <Feed />
-              </Col>
-            <Col xs={12} md={3} lg={4}>
-                <OtherClients />
-            </Col>
-        </Row>
-      {/* About Section */}
-       
+      <Row style={{ color: '#a303a0', height: '570px', overflow: 'hidden' }} className="gx-2">
+        <Col xs={12} md={6} lg={4}>
+          <About authUser={authUser} currentUser={currentUser} />
+        </Col>
+        <Col xs={12} md={6} lg={4}>
+          <Feed />
+        </Col>
+        <Col xs={12} md={12} lg={4}>
+          <OtherClients />
+        </Col>
+      </Row>
     </div>
   );
 };
