862e81919b9015977e813b254d60dba1e3351511
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-07-08 18:35:24 +0100

    update: update the home path to render VisitorClient

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
-// import logo from './logo.svg';
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
+
+import './App.css';
 import ProtectedRoute from './components/ProtectedRoute';
 import Login from './components/Login';
 import User from './components/User';
+import VisitorClient from './components/VisitorClient';
 
 const App: React.FC = () => {
   return (
     <Routes>
     <Route element={<ProtectedRoute />}>
       <Route path="/" element={<div className="App">
-        <User />
+          {/* <User /> */}
+          <VisitorClient/>
       </div>} />
     </Route>
 
