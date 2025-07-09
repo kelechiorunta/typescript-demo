4c04ced8565e05f1b48cbae4c75baed06ad712dc
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-07-09 01:01:23 +0100

    add: add socket.io-client for client connection

--- frontend/src/components/VisitorClient.tsx
+++ frontend/src/components/VisitorClient.tsx
@@ -1,10 +1,12 @@
 // components/VisitorClient.tsx
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useOutletContext } from 'react-router-dom';
+import { io } from 'socket.io-client';
 
 import Sidebar from './Sidebar';
 import UserContent from './UserContent';
+import socket from '../socket-client/socket-client';
 
 // Define the expected type
 export type AuthContextType = {
@@ -22,6 +24,33 @@ const VisitorClient: React.FC = () => {
     const authUser = useOutletContext<AuthContextType>();
     const storedCurrentUser = authUser
     const [currentUser, setCurrentUser] = useState<AuthContextType>(() => ({ ...authUser }));
+
+    useEffect(() => {
+        const host = window.location.hostname;
+        const socketServerURL = 'http://localhost:3700'
+        const socket = io(socketServerURL, {
+            transports: ['websocket'],
+            extraHeaders: {
+              'Authorization': 'Bearer YOUR_TOKEN_HERE',
+              'Content-Type': 'application/json'
+            },
+            withCredentials: true,
+          });
+        socket.on('connect', () => {
+            console.log("Connected to socket.io server")
+        })
+        socket.on('newMessage', (user) => {
+          console.log('Received new user:', user?.username);
+        });
+      
+        return () => {
+            socket.disconnect()
+        }
+        // return () => {
+            
+        //   socket.off('newMessage');
+        // };
+      }, []);
 
   return (
     <Container fluid className="gx-0 " style={{backgroundColor: 'rgba(250,122,82,0.2)'}}>
