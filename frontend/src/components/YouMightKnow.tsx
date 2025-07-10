3e0b0198a9c23b50df2d8af0c0898fe8d6e9cb55
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-07-10 02:06:52 +0100

    fix: add stylings

--- frontend/src/components/YouMightKnow.tsx
+++ frontend/src/components/YouMightKnow.tsx
@@ -11,12 +11,14 @@ const YouMightKnow: React.FC = () => {
   return (
     <Card>
       <Card.Body>
-        <Card.Title className="mb-3">You might know</Card.Title>
+        <Card.Title style={{ width: '90%', textAlign: 'left', margin: 'auto', display: 'inline-block', backgroundColor: 'white', color: ' #a303a0' }} className="mb-3 text-left">
+            <h5>You might know</h5>
+        </Card.Title>
         <ListGroup variant="flush">
           {suggested.map((user, index) => (
             <ListGroup.Item key={index} className="d-flex align-items-center">
               <Image src={user.img} roundedCircle width={40} className="me-3" />
-              <div>
+              <div className="d-flex flex-column align-items-start">
                 <strong>{user.name}</strong>
                 <div className="text-muted" style={{ fontSize: '0.85rem' }}>{user.email}</div>
               </div>
