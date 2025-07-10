dc57a39771e7721a95daa944ae5bb51dd03e2fcc
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-07-10 02:09:19 +0100

    fix: fix props and ui styles

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -13,7 +13,7 @@ const About: React.FC<AboutContextProps> = ({authUser, currentUser}) => {
       <Card style={{ backgroundColor: 'white', color: ' #a303a0' }} className="mb-4">
           <Card.Header style={{ width: 'max-content', display: 'inline-block', backgroundColor: 'white', color: ' #a303a0'}} className='text-left border-0'><strong><h4>About</h4></strong></Card.Header>
     <Card.Body className='text-left'>
-              <p className='gap-4 justify-content-between d-flex align-items-center'><strong>Gender:</strong> {currentUser?.gender || 'Male'}</p>
+      <p className='gap-4 justify-content-between d-flex align-items-center'><strong>Gender:</strong> {currentUser?.gender || 'Male'}</p>
       <p className='gap-4 justify-content-between d-flex align-items-center'><strong>Birthday:</strong> June 26, 1980</p>
       <p className='gap-4 justify-content-between d-flex align-items-center'><strong>Address:</strong> 2239 Hop Camp Road</p>
       <p className='gap-4 justify-content-between d-flex align-items-center'><strong>Email:</strong> { currentUser?.email}</p>
