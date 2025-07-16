2684513910780e14940be434eff63a256fccabff
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-07-16 17:52:59 +0100

    add: update props and state

--- frontend/src/components/VisitorClient.tsx
+++ frontend/src/components/VisitorClient.tsx
@@ -1,114 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col, Placeholder, Spinner } from 'react-bootstrap';
-import { useOutletContext } from 'react-router-dom';
-import io  from 'socket.io-client';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+
 
 import Sidebar from './Sidebar';
-import UserContent from './UserContent';
-import { GET_OTHER_CLIENTS } from '../graphqlClient/queries';
-import { useQuery } from '@apollo/client';
+interface VisitorProps {
+    isOnline: boolean;
+}
 
-// Define the expected type
-export type AuthContextType = {
-  _id: any;
-  username: string;
-  email: string;
-  address: string;
-  picture: string;
-  phone: string;
-  gender: string;
-  birthday: string;
-  backgroundImage: string;
-  isOnline: boolean;
-  videoId: any;
-  backgroundImageId: any;
-  backgroundPlaceholderId: any;
-  occupation: string;
-};
-
-const VisitorClient: React.FC = () => {
-  const authUser = useOutletContext<AuthContextType>();
-  const storedCurrentUser = authUser;
-  const [currentUser, setCurrentUser] = useState<AuthContextType>(authUser);
-//   const [currentUser, setCurrentUser] = useState<AuthContextType>(() => ({ ...authUser }));
-  const [profileUser, setProfileUser] = useState<AuthContextType>(() => ({ ...storedCurrentUser }));
-  const { data, loading, error } = useQuery(GET_OTHER_CLIENTS);
-
-  const [socket, setSocket] = useState<any>(null);
-  const [onlineUsers, setOnlineUsers] = useState<any>(new Set());
-  const [isOnline, setIsOnline] = useState<boolean>(false);
-  const clients = data?.otherClients ?? [];
-
-  useEffect(() => {
-    const socketServerURL = 'http://localhost:3700';
-    const socketInstance = io(socketServerURL, {
-      transports: ['websocket'],
-    //   extraHeaders: {
-    //     Authorization: 'Bearer YOUR_TOKEN_HERE',
-    //     'Content-Type': 'application/json',
-    //   },
-    });
-
-    setSocket(socketInstance);
-
-    socketInstance.on('connect', () => {
-      console.log('Connected to socket.io server');
-    });
-
-    socketInstance.on('newMessage', (user: any) => {
-      console.log('Received new user:', user?.username);
-    });
-
-    return () => {
-      socketInstance.off('newMessage');
-      socketInstance.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!socket || !currentUser) return;
-
-    socket.emit('authenticated', { userId: storedCurrentUser?._id });
-
-    socket.on('userOnline', ({ userId, online }: { userId: any; online: boolean }) => {
-      setOnlineUsers((prev: any) => new Set(prev).add(userId));
-      setIsOnline(online);
-    });
-
-    socket.on('usersOnline', ({ userIds, online }: { userIds: any; online: boolean }) => {
-      setOnlineUsers(new Set(userIds));
-      setIsOnline(online);
-    });
-      
-    socket.on('userOffline', ({ userId }: {userId: any}) => {
-        setOnlineUsers((prev: any) => {
-          const updated = new Set(prev);
-          updated.delete(userId);
-          return updated;
-        });
-      });
-  }, [socket]);
+const VisitorClient: React.FC<VisitorProps> = (
+    {
+      isOnline,
+    }
+) => {
 
   return (
     <Container fluid className="gx-0" style={{ backgroundColor: 'rgba(250,122,82,0.2)' }}>
       <Row className="gx-0">
+        
         <Col xs={12} md={3} lg={2}>
-          <Sidebar active={isOnline} />
+            <Sidebar active={isOnline} />
         </Col>
-
+                
         <Col xs={12} md={9} lg={10}>
-            <UserContent
-              storedUser={storedCurrentUser}
-              authUser={currentUser}
-              currentUser={currentUser}
-              setCurrentUser={setCurrentUser}
-              profileUser={profileUser}
-              setProfileUser={setProfileUser}
-              onlineUsers={onlineUsers}
-              clients={clients}
-              data={data}
-              loading={loading}
-            />
+            <Outlet /> 
         </Col>
       </Row>
     </Container>
